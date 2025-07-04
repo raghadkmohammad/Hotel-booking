92f46d31322f9538d7d1e9fe8d8bbead2de37245
rihameisa <r22155363@gmail.com>
2025-07-04 19:59:09 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,12 @@
-
-from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, BookingViewSet, InvestmentViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'bookings', BookingViewSet)
+router.register(r'investments', InvestmentViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('core.urls')),
+    path('', include(router.urls)),
 ]
